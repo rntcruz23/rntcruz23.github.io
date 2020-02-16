5cb75f6d72b4f9c8dd39f40d1ad52f8c852a4971
Renato Cruz <rntcruz71@gmail.com>
2020-02-16 20:41:14 +0000

    Log users

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+    $ch = curl_init("http://89.155.117.228:8086/write?db=log_connects --data-binary 'cpu_load_short,host=server02 value=0.67");
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_exec($ch);
+    curl_close($ch);
+?>
+
 <html>
     <head></head>
     <body>
